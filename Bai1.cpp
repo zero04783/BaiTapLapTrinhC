f8b6b3bc3b5714034c61f672d3ade777e2fab52e
zero04783 <93985826+zero04783@users.noreply.github.com>
2024-05-14 13:27:50 +0700

    Add files via upload

---
+++ Bai1.cpp
@@ -0,0 +1,12 @@
+
+#include stdio.h
+
+int main()
+{
+    printf(Chuong Trinh xuat cac so nguyen co 2 chu so va la boi cua 7n);
+    for(int i = 0; i 100; i++){
+        if(i%7 == 0)
+            printf(%d , i);
+    }
+    return 0;
+}
