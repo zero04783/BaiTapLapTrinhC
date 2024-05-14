ebd694927ecf2f82473ba21840ee9ad90fa68e6c
zero04783 <93985826+zero04783@users.noreply.github.com>
2024-05-14 13:32:21 +0700

    Update Bai1.cpp

--- Bai1.cpp
+++ Bai1.cpp
@@ -1,12 +1,15 @@
 
-#include stdio.h
+#include <stdio.h>
 
+void xuatBoiCua7Co2ChuSo(){
+    for(int i = 0; i< 100; i++){
+        if(i%7 == 0)
+            printf("%d ", i);
+    }
+}
 int main()
 {
-    printf(Chuong Trinh xuat cac so nguyen co 2 chu so va la boi cua 7n);
-    for(int i = 0; i 100; i++){
-        if(i%7 == 0)
-            printf(%d , i);
-    }
+    printf("Chuong Trinh xuat cac so nguyen co 2 chu so va la boi cua 7\n");
+    xuatBoiCua7();
     return 0;
 }
